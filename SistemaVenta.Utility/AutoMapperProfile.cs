6d94660de23bc2d1f72110b6b3ae464595a1e5c4
alexxware <manuelmorm1@gmail.com>
2025-04-15 21:15:14 -0600

    Creacion primera parte para un mapeo con AutoMapper

---
+++ SistemaVenta.Utility/AutoMapperProfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoMapper;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.Utility
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            
+            CreateMap<Rol, RolDTO>().ReverseMap();
+            CreateMap<Menu, MenuDTO>().ReverseMap();
+            CreateMap<Menu, MenuDTO>().ReverseMap();
+
+            #region Usuario
+            CreateMap<Usuario, UsuarioDTO>()
+                .ForMember(destino =>
+                    destino.RolDescripcion,
+                    opt => opt.MapFrom(origen => origen.IdRolNavigation.Nombre)
+
+                )
+                .ForMember(destino =>
+                    destino.EsActivo,
+                    opt => opt.MapFrom(origen => origen.EsActivo == true ? 1 : 0)
+                );
+            #endregion
+        }
+    }
+}
